868d95c72bd80d905257ee1c1740546218c839d6
kwak9898 <kwak9898@gitbub.com>
2022-12-19 17:41:39 +0900

    Create S3 Bucket

--- lib/aws-cdk-stack.ts
+++ lib/aws-cdk-stack.ts
@@ -1,16 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { aws_s3 as s3 } from 'aws-cdk-lib';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new s3.Bucket(this, 'aws-s3-sandbox-cdktemp-dev', {
+      versioned: true,
+    });
   }
 }
